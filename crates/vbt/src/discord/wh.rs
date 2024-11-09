8e0448a421297cad2491355f0623eb488db6f65f
Lilith Iris <itslirissama@gmail.com>
2024-11-09 12:10:25 +0700

    change weekly to daily

--- crates/vbt/src/discord/wh.rs
+++ crates/vbt/src/discord/wh.rs
@@ -1,7 +1,7 @@
 use std::env;
 
-pub fn weekly_url() -> Result<String, env::VarError> {
-    env::var("WEEKLY")
+pub fn daily_url() -> Result<String, env::VarError> {
+    env::var("DAILY")
 }
 
 pub fn processed_url() -> Result<String, env::VarError> {
